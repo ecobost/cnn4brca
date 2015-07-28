eb4c0bb676c82a837674aa83eeb71402f19a83d1
Erick Cobos <ecobos21@hotmail.com>
2015-07-28 18:52:58 -0500

    Advanced, almost done. couple of Todos, works.

--- experiments/ImageRetrieval/retrieval.py
+++ experiments/ImageRetrieval/retrieval.py
@@ -3,10 +3,11 @@
 '''
 Methods to generate small image patches from full mammograms. Justification for most decisions taken are provided in the thesis. Run as:
 	python3 retrieval
-Methods could also be used independently.
+
+Methods createMassPatches and createMCCPatches could also be used independently.
 
 Configuration params
--------
+-----------------
 _path: string
 	Directory containing the mammograms. Default: .
 _recursive: boolean (!Not yet working)
@@ -24,7 +25,17 @@ _maxBlackAllowed: int
 	will be deleted. Default: 0.25
 _grayValues: float
 	Number of intensity values the patch will have after constrast stretching. 
-	Default: 255
+	Default: 65535
+
+Notes
+---------------
+Developed in python 3.x. Needs changes to work in previous versions.
+ 
+Uses PILLOW 3.0. Will not work with PIL.
+
+Uses 64-bit floats to calculate the mean and variance statistics, 32-bit machines
+may lose precision. Using images with depth 19 or more will cause lose of precision
+when calculating the variance; 8-bit or 16-bit images are alright.
 
 '''
 _path = "."
@@ -34,14 +45,13 @@ _extOut = ".pch"
 _originalPixelSize = 0.05
 _patchSizeInPixels = 127
 _maxBlackAllowed = 0.25
-_grayValues = 255
+_grayValues = 65535
 
 import glob
-import Image from PIL
-import numpy as np
 import os
+import numpy as np
 import scipy.misc
-
+from PIL import Image
 
 
 def createPatches(patchSizeInMm, stride, subfolder, path = _path, ext = _ext,
@@ -51,7 +61,7 @@ def createPatches(patchSizeInMm, stride, subfolder, path = _path, ext = _ext,
 	'''
 	Creates smaller images (patches) by sliding a window over big images.
 	
-	Creates a subfolder to store the patches of every image in the path directory. Crops the image according to the patchSize and originalPixelSize. Assigns its respective labels and resizes it to pacthSizeInPixels pixels. Accumulates some overall statistics (mean and std) for feature normalization (later).
+	Creates a subfolder to store the patches of every image in the path directory. Crops the image according to the patchSize and originalPixelSize. Assigns its respective labels and resizes it to pacthSizeInPixels pixels. Accumulates some overall statistics (mean and var) for feature normalization (later).
 	
 	Parameters
 	-------
@@ -83,16 +93,19 @@ def createPatches(patchSizeInMm, stride, subfolder, path = _path, ext = _ext,
 
 	# Create subfolder
 	os.chdir(path)
-	os.mkdir(subfolder)
+	os.makedirs(subfolder, exist_ok = True)
 
-	# Calculate the number of pixels that represent the patch and stride in
-	# the original big images.
+	# Calculate the number of pixels that represent the patch and stride in the 		# original big images.
 	bigPatchSize = round(patchSizeInMm / originalPixelSize)
 	bigStride = round(stride / originalPixelSize)
 
-	# Initialize the mean and std statistics
+	# Initialize the mean and var statistics
 	overallMean = np.zeros( (patchSizeInPixels, patchSizeInPixels) )
-	overallStd = np.zeros( (patchSizeInPixels, patchSizeInPixels) )
+	overallVar = np.zeros( (patchSizeInPixels, patchSizeInPixels) )
+
+	# Initialize other statistics
+	numberOfPatches = 0
+	numberOfImages = 0
 
 	# Recursivity could be added in here: a for over all folders (os.walk) plus chdir to the new directory. To decide: should a new subfolder be created or everything added into the big subfolder (change accordingly), what about the statistics in each subfolder or outside everything (if there is many folders). If various subfolders are going to be used we could just call it recursively (as a depth-frist tree traversal) before the subfolder creation.
 
@@ -108,8 +121,10 @@ def createPatches(patchSizeInMm, stride, subfolder, path = _path, ext = _ext,
 		# Create copy of image with black spaces (0-4) as True, else False
 		blackImage = (image <= 4)
 
-		# Initialize 3-d volume where patches are going to be contained
-#TODO: How to initialize. Don't know the final size(amount of strides could be calculated but black spaces get skipped)		imagePatches = 
+		# Initialize lists for patches and labels. I use a list because
+		# append works faster. Later it is transformed to ndarray.
+		imagePatches = []
+		imageLabels = []
 
 		# Slide window across x (rows) and y (columns) of image. 
 		M, N = image.shape
@@ -128,26 +143,69 @@ def createPatches(patchSizeInMm, stride, subfolder, path = _path, ext = _ext,
 
 				# Background reduction and contrast stretching
 				patch = adjustContrast(patch, grayValues)
-	
-#TODO Write this method		# Assign label (?)
 
-				# Reduce
+#TODO Write this method		# Assign label
+				label = np.array([0, 0, 0]);
+
+				# Reduce.  If mode is not provided to toimage it is
+				# set to 'L' (uint8 grayscale).
 				size = (patchSizeInPixels, patchSizeInPixels)
-				patch = patch.resize(size, Image.LANCZOS)
-				patch = np.array(patch)
+				patchIm = scipy.misc.toimage(patch, mode = 'F')
+				patchIm = patchIm.resize(size, Image.LANCZOS)
+				patch = np.array(patchIm)
 
-				# Append (patch, label) per image on 3-d matrix
-#TODO From here on		How to initialize. use append or insert (both inefficient). Lists are more efficient. Maybe a list of patches. calculating mean is harder though but maybe i can convert back to a 3-d array. How inefficient is this though
+				# Append
+				imagePatches.append(patch)
+				imageLabels.append(label)
+		
+		# Transform lists into numpy arrays. Patches are stored as rows.
+		# Patch x can be accesed as imagePatches[x,:,:], same with labels.
+		imagePatches = np.array(imagePatches)
+		imageLabels = np.array(imageLabels) 
 
-		#Accumulate the mean and std of patch values in image so that we can normalize per feature on entire database. Runing average.
+		# Update the number of patches and number of images.
+		oldNumberOfPatches = numberOfPatches
+		numberOfPatches += imagePatches.shape[0]
+		numberOfImages += 1
+		
+		# Update the overall mean as a running average.
+		tmpSum = np.sum(imagePatches, axis = 0, dtype = np.float64)
+		overallMean = (overallMean * (oldNumberOfPatches / numberOfPatches) 					+ tmpSum / numberOfPatches)
 
-		#save 3-d .pch on disk.
-		#im.save(fileName + extOut, "JPEG")
+		# Update the overall variance as a running average. Not exact because
+		# the mean is only an approximation up to this point.
+		tmpSum = np.sum( (imagePatches - overallMean) ** 2, axis = 0,
+				dtype = np.float64)
+		overallVar = (overallVar * (oldNumberOfPatches / numberOfPatches) 
+				+ tmpSum / numberOfPatches)
+
+#TODO Still don't like the accuracy. Compare with Welford method (https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance). Use 4 images and see which one gives a more exact approx to the real var.
+
+
+		# Save image patches and labels to subfolder.
+		np.save(subfolder + "/" + fileName + "_pch", imagePatches)
+		np.save(subfolder + "/" + fileName + ".lbl", imageLabels)
+#TODO: extOut, maybe let it as .npy or extOut.npy. Waht about extension for labels. add _labelSuffix = "L". 
+# mammogram4.pch.npy or mammogram4_pch.npy or mammogram4.npy
+# if i say glob(.npy) it wil be hard to get both npy and L, it may be that some pictures are named_L. Maybe it is better to put another extension because it is easier to do pattern matchiong.
 	
 		# Print a signal (.) to show that it is still running
 		print('.', end = "", flush = True)
 	
-	#Save the mean and std of all images on disk (for later normalization of test and entire mammogram).
+	# Recursivity: END here.
+
+	# Save statistics(mean and var)
+	np.save("overallMean", overallMean)
+	np.save("overallVar", overallVar)
+	
+	# Print some statistics
+	print("\nPatches were succesfully generated!")
+	print("Processed images:", numberOfImages)
+	print("Total number of patches:", numberOfPatches)
+	print("Average mean of patches:")
+	print(overallMean)
+	print("Average variance of patches:")
+	print(overallVar)
 
 	# Change back to old directory
 	os.chdir(oldPath)
@@ -158,7 +216,8 @@ def adjustContrast(array, newMax):
 	"""
 	Performs background reduction by substracting the mean of the image and
 	stretches the contrast (linear normalization) to cover the entire range
-	of pixel values 0-newMax (newMax = 255 for an 8-bit image).
+	of pixel values 0-newMax (newMax = 255 for an 8-bit image, newMax = 6555
+	for a 16-bit image, etc.)
 
 	Parameters
 	----------------
@@ -181,6 +240,7 @@ def adjustContrast(array, newMax):
 	# To produce float64 results, comment it to preserve input format.
 	# result = array.astype('float')
 	
+	# To avoid modifying the input array
 	result = array.copy()
 	
 	# Background reduction
@@ -222,5 +282,19 @@ if __name__ == "__main__":
 	createMassPatches()
 	createMCCPatches()
 
-#friday 1:40
-# Too many pacthes. May need slightly bigger stride (3 mm will probably do). Need from 500-1000 patches per mamogram. Maybe it's better to generate more than 1000 (small stride) and drop some normal ones to upsample the lessions (Nop, way too many images of the same lession, just translated, overfitting). Maybe put 2mm for microcalc, too.
+# friday 6:55, 2:45
+# Too many patches. May need slightly bigger stride (3 mm will probably do). Need from 500-1000 patches per mamogram. Maybe it's better to generate more than 1000 (small stride) and drop some normal ones to upsample the lessions (Nop, way too many images of the same lession, just translated, overfitting). Maybe put 2mm for microcalc, too.
+# Maybe use uint8/uint16 format instead of float32 for weight.
+# 2234 127x127 patches in uint8 36MB, float32 144MB. Exactly 4 times more.
+'''
+Tests: (already performed)
+	Does not delete or crash if subfolder already exists. It ignores it.
+	Overwrites files if they already exist.
+	It does deletes patches with more than 25% black.
+	It prints format32
+
+Tests (to do):
+	Mean and var work. Errors in the 1*e-5 ratio. 
+	check other online var and mean.
+	Works with various images
+'''
